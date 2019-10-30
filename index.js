bfa4fc0dd16025a3c320974b617f74d6a60a8598
Eric Siu <siueric0010@gmail.com>
2019-10-29 22:44:53 -0700

    Just finished video #6.5

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
-
+const routes = require('./routes/api'); // knows it's js, so not need for .js
 // set up express
 const app = express();
 
-// request paramter, response paramter
-// request = info about the request
-// response = info about the response to the client who made the get request
-// /api is the route (localhost:4000/api)
-// could get post/delete/put by using the app.
-app.get('/api', function(req, res) {
-  console.log("GET request");
-  res.send({name: 'Name'})
-});
+// Initiliaze routes
+app.use('/api', routes); // the app will use the route '/api' adds the api route
+// or app.use('/api', require('./routes/api')) --> .use to use middleware
+// (code that runs between request and response)
+
+
+
+
 
 
 
@@ -22,3 +21,16 @@ app.listen(process.env.port || 4000, function() {
 });
 // Listen for port 4000 --> made up
 // Heroku/ hosting service gives a environment variable: process.env.port
+
+/*
+// request paramter, response paramter
+// request = info about the request
+// response = info about the response to the client who made the get request
+// /api is the route (localhost:4000/api)
+// could get post/delete/put by using the app.
+app.get('/api', function(req, res) {
+  console.log("GET request");
+  res.send({name: 'Name'})
+});
+
+*/
---
+++ routes/api.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const router = express.Router();
+
+
+
+// get request: list of ninjas from database (only 1 that can be tested in browser bc browser only can send get requests)
+router.get('/ninjas', function(req,res) {
+  res.send({type:'GET'}); // sends object saying what request they made
+});
+
+// post request: add ninja to DB
+router.post('/ninjas', function(req,res) {
+  res.send({type:'POST'}); // sends object saying what request they made
+});
+
+// update ninja in the db (id is a parameter that is required to get the right ninja)
+router.put('/ninjas/:id', function(req,res) {
+  res.send({type:'PUT'}); // sends object saying what request they made
+});
+
+// delete a ninja from the db
+router.delete('/ninjas/:id', function(req,res) {
+  res.send({type:'DELETE'}); // sends object saying what request they made
+});
+
+module.exports  = router; // exports the thing so it can be imported in another file (index.js)
